1739b34ffbaed9fdfb83fabbec4fe3b0cde1ca25
Giel Vanvelk <giel.vanvelk@student.kuleuven.be>
2021-03-09 21:35:16 +0100

    Initial Commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/giel/etasl/ws/my_new_workspace/src
+# Build directory: /home/giel/etasl/ws/my_new_workspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
